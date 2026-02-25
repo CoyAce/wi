9c642f6c17c2994c0c50caf30066a15763c58208
CoyAce <akeycoy@gmail.com>
2026-02-25 22:55:05 +0800

    server: add timeout log

--- server.go
+++ server.go
@@ -380,5 +380,6 @@ func (s *Server) dispatch(addr string, bytes []byte, block uint32) {
 		case <-time.After(s.Timeout):
 		}
 	}
-	log.Printf("[%s] write timeout after %d retries, type %v", addr, s.Retries, bytes[:2])
+	sign, _ := s.addrMap.Load(addr)
+	log.Printf("[%s] write timeout after %d retries, type %v, block %v sign %v", addr, s.Retries, bytes[:2], block, sign)
 }
